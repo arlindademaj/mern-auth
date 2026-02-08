d0a5a248e4994576e90e6739c473ecf8d12684b9
arlindademaj <aarlindademaj@gmail.com>
2026-02-08 17:43:57 +0100

    correct method naming and improve typings

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -7,7 +7,12 @@ export interface UserDocument extends mongoose.Document {
   verified: boolean;
   createdAt: Date;
   updatedAt: Date;
+  __v: number;
   comparePassword(val: string): Promise<boolean>;
+  omitPassword(): Pick<
+    UserDocument,
+    "_id" | "email" | "verified" | "createdAt" | "updatedAt" | "__v"
+  >;
 }
 
 const userSchema = new mongoose.Schema<UserDocument>(
@@ -31,6 +36,12 @@ userSchema.pre("save", async function () {
 
 userSchema.methods.comparePasswords = async function (val: string) {
   return compareValue(val, this.password);
+};
+
+userSchema.methods.omitPassword = function () {
+  const user = this.toObject();
+  delete user.password;
+  return user;
 };
 
 const userModel = mongoose.model<UserDocument>("User", userSchema);
