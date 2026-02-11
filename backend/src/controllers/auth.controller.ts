2ed5f8a02350c28e7d35575d3bcca1f36aca0e31
arlindademaj <aarlindademaj@gmail.com>
2026-02-11 21:39:00 +0100

    add auth register and login handlers

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,20 +1,9 @@
 import { z } from "zod";
 import catchErrors from "../utils/catchErrors";
-import { createAccount } from "../services/auth.service";
-import { CREATED } from "../constants/http";
+import { createAccount, loginUser } from "../services/auth.service";
+import { CREATED, OK } from "../constants/http";
 import { setAuthCookies } from "../utils/cookies";
-
-const registerSchema = z
-  .object({
-    email: z.string().email().min(5).max(255),
-    password: z.string().min(6).max(255),
-    confirmPassword: z.string().min(6).max(255),
-    userAgent: z.string(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Password does not match",
-    path: ["confirmPassword"], // fixed
-  });
+import { loginSchema, registerSchema } from "./auth.schemas";
 
 export const registerHandler = catchErrors(async (req, res) => {
   const request = registerSchema.parse({
@@ -30,4 +19,17 @@ export const registerHandler = catchErrors(async (req, res) => {
   setAuthCookies({ res, accessToken, refreshToken });
 
   return res.status(CREATED).json(user);
+});
+
+export const loginHandler = catchErrors(async (req, res) => {
+  const request = loginSchema.parse({
+    ...req.body,
+    userAgent: String(req.headers["user-agent"] || ""),
+  });
+
+  const { accessToken, refreshToken } = await loginUser(request);
+  setAuthCookies({ res, accessToken, refreshToken });
+  return res.status(OK).json({
+    message: "Login successful",
+  });
 });
