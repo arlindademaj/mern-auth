35b93091d3c53cdda02b0a4db6f323a1f641e466
arlindademaj <aarlindademaj@gmail.com>
2026-02-20 22:29:01 +0000

    improve typing and fix verify options compatibility

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,4 +1,4 @@
-import jwt, { SignOptions } from "jsonwebtoken";
+import jwt, { SignOptions, VerifyOptions } from "jsonwebtoken";
 import { SessionDocument } from "../models/session.model";
 import { UserDocument } from "../models/user.model";
 import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
@@ -16,9 +16,9 @@ type SignOptionsAndSecret = SignOptions & {
   secret: string;
 };
 
-const defaults: SignOptions = {
-  audience: ["user"],
-};
+const defaults = {
+  audience: "user",
+} as const;
 
 const accessTokenSignOptions: SignOptionsAndSecret = {
   expiresIn: "15m",
@@ -39,4 +39,22 @@ export const signToken = (
     ...defaults,
     ...signOpts,
   });
+};
+
+export const verifyToken = <TPayload extends object = AccessTokenPayload>(
+  token: string,
+  options?: VerifyOptions & { secret?: string },
+) => {
+  const { secret = JWT_SECRET, ...verifyOpts } = options || {};
+
+  try {
+    const payload = jwt.verify(token, secret, {
+      ...defaults,
+      ...verifyOpts,
+    }) as TPayload;
+
+    return { payload };
+  } catch (error: any) {
+    return { error: error.message };
+  }
 };
