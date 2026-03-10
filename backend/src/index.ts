b00efd2d823d8cfad20ed3bd1997b95da9267038
arlindademaj <aarlindademaj@gmail.com>
2026-03-10 14:08:52 +0000

    feat: setup express server with middleware and routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,18 @@
 import "dotenv/config";
+import express from "express";
 import cors from "cors";
-import express, { urlencoded } from "express";
-import connectToDatabase from "./config/db";
-import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
-import { extend } from "zod/v4/core/util";
 import cookieParser from "cookie-parser";
+import connectToDatabase from "./config/db";
 import errorHandler from "./middleware/errorHandler";
-import catchErrors from "./utils/catchErrors";
-import { OK } from "./constants/http";
+import authenticate from "./middleware/authenticate";
 import authRoutes from "./routes/auth.route";
+import userRoutes from "./routes/user.route";
+import sessionRoutes from "./routes/session.route";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
 
 const app = express();
 
+// add middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -22,16 +23,24 @@ app.use(
 );
 app.use(cookieParser());
 
-app.get("/", (req, res, next) => {
-  res.status(OK).json({
-    emri: "Arlind",
+// health check
+app.get("/", (_, res) => {
+  return res.status(200).json({
+    status: "healthy",
   });
 });
 
+// auth routes
 app.use("/auth", authRoutes);
 
+// protected routes
+app.use("/user", authenticate, userRoutes);
+app.use("/sessions", authenticate, sessionRoutes);
+
+// error handler
+app.use(errorHandler);
+
 app.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT} in ${NODE_ENV} enviroment`);
+  console.log(`Server listening on port ${PORT} in ${NODE_ENV} environment`);
   await connectToDatabase();
 });
-app.use(errorHandler);
