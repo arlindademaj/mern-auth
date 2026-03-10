bd914f347335010789b4941e134180ec5c99f083
arlindademaj <aarlindademaj@gmail.com>
2026-03-10 14:10:34 +0000

    feat: add reset password schema validation

--- backend/src/controllers/auth.schemas.ts
+++ backend/src/controllers/auth.schemas.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const emailSchema = z.string().email().min(5).max(255);
-const passwordSchema = z.string().min(6).max(255);
+export const passwordSchema = z.string().min(6).max(255);
 
 export const loginSchema = z.object({
   email: emailSchema,
@@ -19,3 +19,8 @@ export const registerSchema = loginSchema
   });
 
 export const verificationCodeShcema = z.string().min(1).max(24);
+
+export const resetPasswordSchema = z.object({
+  password: passwordSchema,
+  verificationCode: verificationCodeShcema,
+});
